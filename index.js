6303d8e5ce0becd3ef3f7278af02c5a87c4af224
DURGAPRASAD <durgaprasad@ipexzoom.com>
2016-12-19 23:21:44 +0530

    V 1

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/convert/csv/to/json', function(request, response) {
     var q = request.query.q;
 
